5e58eb10309850a0ad21fa34e259b1fdd537d359
Himesh Shiwantha <shiwanthah@gmail.com>
2023-02-21 20:42:57 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,44 @@
+const foods = [
+    {
+        name:"burger",
+        type:"breakfast",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price:19.99,
+        image:"./images/burger.jpg"
+    },
+    {
+        name:"sandwich",
+        type:"breakfast",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price:14.99,
+        image:"./images/sandwich.jpg"
+    },
+    {
+        name:"pizza",
+        type:"dinner",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price:24.59,
+        image:"./images/pizza.jpg"
+    },
+    {
+        name:"ice cream",
+        type:"dessert",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price:9.99,
+        image:"./images/icecream.jpg"
+    },
+    {
+        name:"milkshake",
+        type:"beverage",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price:10.99,
+        image:"./images/milkshake.jpg"
+    },
+    {
+        name:"smoothie",
+        type:"beverage",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price:19.99,
+        image:"./images/smoothie.jpg"
+    },
+]
